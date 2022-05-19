5c55a76b2ebab68dc679f64aa46e3a81d49989e8
Kaka Gaming <shizurakaori@gmail.com>
2022-05-19 07:18:58 +0700

    update

--- resources/views/backend/kecamatan/index.blade.php
+++ resources/views/backend/kecamatan/index.blade.php
@@ -8,43 +8,42 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="header-title"><b>Import Data Kecamatan</b></h4>
-                        <button type="button" class="btn btn-primary mb-3 pull-right">Import Data Kecamatan</button>
-                        <button type="button" class="btn btn-outline-primary mb-3 mr-3 pull-right">Download To Excel</button>
+                        <button type="button" class="btn btn-primary mb-3 pull-right"><i class="ti-upload"> </i> Import Data Kecamatan</button>
+                        <button type="button" class="btn btn-outline-primary mb-3 mr-3 pull-right"><i class="ti-download"></i> Download To Excel</button>
 
                         <div class="single-table">
                             <div class="table-responsive">
                                 <table class="table table-hoverable">
                                     <thead>
                                         <tr>
-                                            <th scope="col">Province </th>
-                                            <th scope="col">Kota</th>
-                                            <th scope="col">Kelurahan</th>
-                                            <th scope="col">Kecamatan</th>
+                                            
+                                            <th scope="col">Province <button class="btn btn-xs pull-right"><i class="ti-arrow-down"></i></button></th>
+                                            <th scope="col">Kota <button class="btn btn-xs pull-right"><i class="ti-arrow-down"></i></button></th>
+                                            <th scope="col">Kelurahan <button class="btn btn-xs pull-right"><i class="ti-arrow-down"></i></button></th>
+                                            <th scope="col">Kecamatan <button class="btn btn-xs pull-right"><i class="ti-arrow-down"></i></button></th>
                                         </tr>
                                         <tr>
                                             <th>
                                                 <form class="nosubmit">
-                                                    <input class="nosubmit" type="search" placeholder="Search...">
+                                                    <input class="nosubmit" type="search" placeholder="Cari...">
                                                 </form>
                                             </th>
                                             <th>
                                                 <form class="nosubmit">
-                                                    <input class="nosubmit" type="search" placeholder="Search...">
+                                                    <input class="nosubmit" type="search" placeholder="Cari...">
                                                 </form>
                                             </th>
                                             <th>
                                                 <form class="nosubmit">
-                                                    <input class="nosubmit" type="search" placeholder="Search...">
+                                                    <input class="nosubmit" type="search" placeholder="Cari...">
                                                 </form>
                                             </th>
                                             <th>
                                                 <form class="nosubmit">
-                                                    <input class="nosubmit" type="search" placeholder="Search...">
+                                                    <input class="nosubmit" type="search" placeholder="Cari...">
                                                 </form>
                                             </th>
-                                            
                                         </tr>
-                                    </thead>
                                     </thead>
                                     <tbody>
                                         @foreach ($kecamatan as $kecamatans)
@@ -53,15 +52,15 @@
                                             <td>{{ $kecamatans->kota }}</td>
                                             <td>{{ $kecamatans->kelurahan }}</td>
                                             <td>{{ $kecamatans->kecamatan }}</td>
-                                            <td>
+                                            <td class="pull-right">
                                                 <div class="col clearfix">   
                                                     <button class="btn btn-light " data-toggle="dropdown"><i class="ti-more-alt"></i></button>
                                                     <div class="dropdown-menu">
                                                         <form action="{{ route('kecamatan.destroy',$kecamatans->id) }}" method="POST">
-                                                            <a class="dropdown-item" href="{{ route('kecamatan.edit',$kecamatans->id) }}">Edit</a>
+                                                            <a class="dropdown-item" href="{{ route('kecamatan.edit',$kecamatans->id) }}"><i class="ti-pencil-alt"></i> Ubah</a>
                                                             {!! csrf_field() !!}
                                                             <input name="_method" type="hidden" value="DELETE">
-                                                            <a class="dropdown-item" href="#" onclick="parentNode.submit(); return false;">delete</a>
+                                                            <a class="dropdown-item" href="#" onclick="parentNode.submit(); return false;"><i class="ti-trash"></i> Hapus</a>
                                                         </form>
                                                     </div>
                                                 </div>
@@ -70,6 +69,45 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                <br>
+                                <div class="row" style="margin-top: 23px;">
+                                    <div class="col-sm-6">
+                                        <div class="dropdown">
+                                            <span>Items per page</span>
+                                            <button class="btn btn-light dropdown-toggle" type="button" data-toggle="dropdown">
+                                                10
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item" href="#">20</a>
+                                                <a class="dropdown-item" href="#">30</a>
+                                                <a class="dropdown-item" href="#">40</a>
+                                            </div>
+                                    
+                                            {{ $kecamatan->firstItem() }}
+                                            -
+                                            {{ $kecamatan->lastItem() }}
+                                            of
+                                            {{ $kecamatan->total() }}
+                                            items
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="pull-right">
+                                            {{ $kecamatan->links() }} 
+                                        </div>
+                                        <div class="pull-right" style="margin-right: 30px;">
+                                                <button class="btn btn-light" type="button" data-toggle="dropdown">
+                                                    {{ $kecamatan->currentPage() }}
+                                                </button>
+                                                <div class="dropdown-menu">
+                                                    <a class="dropdown-item" href="#">20</a>
+                                                    <a class="dropdown-item" href="#">30</a>
+                                                    <a class="dropdown-item" href="#">40</a>
+                                                </div>
+                                            of {{ $kecamatan->lastPage() }} pages
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
---
+++ resources/views/vendor/pagination/default.blade.php
@@ -0,0 +1,36 @@
+@if ($paginator->hasPages())
+    <ul class="pagination">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="page-item disabled"><span class="page-link" style="background-color: black; color:white; margin-right: 30px;"><i class="fa fa-chevron-left"></i></span></li>
+        @else
+            <li class="page-item"><a class="page-link" style="background-color: black; color:white; margin-right: 30px;" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="fa fa-chevron-left"></i></a></li>
+        @endif
+
+        {{-- Pagination Elements --}}
+        {{-- @foreach ($elements as $element) --}}
+            {{-- "Three Dots" Separator --}}
+            {{-- @if (is_string($element))
+                <li class="disabled"><span>{{ $element }}</span></li>
+            @endif --}}
+
+            {{-- Array Of Links --}}
+            {{-- @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="active"><span>{{ $page }}</span></li>
+                    @else
+                        <li><a href="{{ $url }}">{{ $page }}</a></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach --}}
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li class="page-item"><a class="page-link" style="background-color: black; color:white;" href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="fa fa-chevron-right"></i></a></li>
+        @else
+            <li class="page-item disabled"><span class="page-link" style="background-color: black; color:white;"><i class="fa fa-chevron-right"></i></span></li>
+        @endif
+    </ul>
+@endif
